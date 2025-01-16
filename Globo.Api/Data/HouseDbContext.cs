ae0ecac140d218c1b39af923b8cc036297709027
Alvin Ramirez <alvin.ramirez48@gmail.com>
2025-01-16 23:49:54 +0100

    feat: configure HouseDbContext to use SQLite database
    - Add OnConfiguring method to specify SQLite as the database provider
    - Store the SQLite database file in the local application data folder

--- Globo.Api/Data/HouseDbContext.cs
+++ Globo.Api/Data/HouseDbContext.cs
@@ -3,4 +3,12 @@ using Microsoft.EntityFrameworkCore;
 public class HouseDbContext: DbContext
 {
     public DbSet<HouseEntity> Houses => Set<HouseEntity>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        optionsBuilder
+            .UseSqlite($"Data Source={Path.Join(path, "house.db")}");
+    }
 }
