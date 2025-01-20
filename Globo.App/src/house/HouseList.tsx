6fd0259e4498a5e597cfd5dabf7853464c7a8bd8
Alvin Ramirez <alvin.ramirez48@gmail.com>
2025-01-21 00:02:00 +0100

    feat: add initial HouseList component with state management for houses

---
+++ Globo.App/src/house/HouseList.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import { House } from "../types/house";
+
+const HouseList = () => {
+    const [houses, setHouses] = useState<House[]>([]);
+}
+
+export default HouseList;
