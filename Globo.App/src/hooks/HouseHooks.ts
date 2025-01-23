db712df4bc33e4374a1249c195fb239fb18a4a52
Alvin Ramirez <alvin.ramirez48@gmail.com>
2025-01-23 23:58:21 +0100

    feat: add custom hook useFetchHouses for fetching and managing house data

---
+++ Globo.App/src/hooks/HouseHooks.ts
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { House } from "../types/house";
+import config from "../config";
+
+const useFetchHouses = (): House[] => {
+    const [houses, setHouses] = useState<House[]>([]);
+
+    const fetchHouses = async () => {
+        const rsp = await fetch(`${config.baseApiUrl}/houses`);
+        const houses = await rsp.json();
+        setHouses(houses);
+    };
+    fetchHouses();
+
+    return houses;
+}
+
+export default useFetchHouses;
