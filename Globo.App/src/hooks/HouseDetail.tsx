852c9cf07e1ae4ace2d0f78440ba7cea21bb4ead
Alvin Ramirez <alvin.ramirez48@gmail.com>
2025-02-02 17:36:50 +0100

    feat: add HouseDetail component to fetch and display house details by ID

---
+++ Globo.App/src/hooks/HouseDetail.tsx
@@ -0,0 +1,18 @@
+import { useParams } from "react-router-dom";
+import { useFetchHouse } from "./HouseHooks";
+import ApiStatus from "../apiStatus";
+
+const HouseDetail = () => {
+    const { id } = useParams();
+    if (!id) throw Error("House id not found")
+    const houseId = parseInt(id);
+
+    const { data, status, isSuccess} = useFetchHouse(houseId);
+    if (!isSuccess) return <ApiStatus status={status}/>
+    if (!data) return <div>House not found</div>
+    return (
+        <></>
+    );
+};
+
+export default HouseDetail;
