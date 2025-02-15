8b15ebe2868fe17c4bc79e3ff2d950f9f5ee29a9
Alvin Ramirez <alvin.ramirez48@gmail.com>
2025-02-15 23:34:09 +0100

    fix: fix subtitle prop and add navigation to home

--- Globo.App/src/main/Header.tsx
+++ Globo.App/src/main/Header.tsx
@@ -1,6 +1,12 @@
+import { useNavigate } from 'react-router-dom';
 import logo from './GloboLogo.png'
 
-const Header = () => {
+type Args = {
+    subtitle: string;
+}
+
+const Header = ({ subtitle }: Args) => {
+    const nav = useNavigate();
     return (
         <header className="row mb-4">
             <div className="col-5">
@@ -8,11 +14,12 @@ const Header = () => {
                     src={logo}
                     className="logo"
                     alt="logo"
+                    onClick={() => nav("/")}
                 />
             </div>
-            <div className="col-7 mt-5 subtitle"></div>
+            <div className="col-7 mt-5 subtitle">{subtitle}</div>
         </header>
     );
-}
+};
 
 export default Header
