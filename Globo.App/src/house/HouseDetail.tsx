394ab6b20924d8859f57bdb7828b855b7b6d8d11
Alvin Ramirez <alvin.ramirez48@gmail.com>
2025-02-18 23:28:07 +0100

    feat: add edit and delete buttons to house detail page

--- Globo.App/src/house/HouseDetail.tsx
+++ Globo.App/src/house/HouseDetail.tsx
@@ -1,5 +1,5 @@
-import { useParams } from "react-router-dom";
-import { useFetchHouse } from "./HouseHooks";
+import { Link, useParams } from "react-router-dom";
+import { useFetchHouse } from "../hooks/HouseHooks";
 import ApiStatus from "../apiStatus";
 import { currencyFormatter } from "../config";
 
@@ -12,7 +12,7 @@ const HouseDetail = () => {
     if (!isSuccess) return <ApiStatus status={status}/>
     if (!data) return <div>House not found</div>
     return (
-        <div className="row">
+    <div className="row">
       <div className="col-6">
         <div className="row">
           <img
@@ -20,6 +20,27 @@ const HouseDetail = () => {
             src={data.photo ? data.photo : defaultImage}
             alt="House pic"
           />
+        </div>
+        <div className="row mt-3">
+          <div className="col-2">
+            <Link
+              className="btn btn-primary w-100"
+              to={`/house/edit/${data.id}`}
+            >
+              Edit
+            </Link>
+          </div>
+          <div className="col-2">
+            <button
+              className="btn btn-danger w-100"
+              onClick={() => {
+                if (window.confirm("Are you sure?"))
+                  deleteHouseMutation.mutate(data);
+              }}
+            >
+              Delete
+            </button>
+          </div>
         </div>
       </div>
       <div className="col-6">
