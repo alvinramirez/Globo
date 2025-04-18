34fb9e91472c392447a7daedec953a839561d2e9
Alvin Ramirez <alvin.ramirez48@gmail.com>
2025-04-18 16:27:31 +0200

    refactor: replace Container with Box for simplified layout control

--- Globo.App/src/main/Layout.tsx
+++ Globo.App/src/main/Layout.tsx
@@ -1,24 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
-import { Container } from "@mui/material";
+import { Box } from "@mui/material";
 
 const Layout = () => {
     return (
         <>
             <Header subtitle="Providing houses all over the world" />
-            <Container 
-                maxWidth="md"
-                sx={{ 
-                    mt: 16, 
-                    mb: 4,
-                    mx: "auto",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center", 
-                }}
-            >
+            <Box sx={{ mt: 16, mb: 4 }}>
                 <Outlet />
-            </Container>
+            </Box>
         </>
     );
 };
