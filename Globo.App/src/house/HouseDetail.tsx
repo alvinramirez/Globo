607757c29a0b143ba5fbec90dfa92e0646fdad53
Alvin Ramirez <alvin.ramirez48@gmail.com>
2025-04-18 19:42:24 +0200

    style: wrap content in Container to center layout and match global structure

--- Globo.App/src/house/HouseDetail.tsx
+++ Globo.App/src/house/HouseDetail.tsx
@@ -14,6 +14,7 @@ import {
   CardMedia,
   CardContent,
   Stack,
+  Container,
 } from "@mui/material";
 
 const HouseDetail = () => {
@@ -30,63 +31,62 @@ const HouseDetail = () => {
     console.log("Casa cargada:", data);
 
     return (
-      <Box sx={{ mt: 4 }}>
-      <Grid container spacing={4}>
-        {/* Imagen y botones */}
-        <Grid item xs={12} md={6}>
-          <Card>
-            <CardMedia
-              component="img"
-              height="250"
-              image={data.photo || defaultImage}
-              alt="House pic"
-              sx={{ objectFit: "cover" }}
-            />
-            <CardContent>
-              <Stack direction="row" spacing={2}>
-                <Button
-                  fullWidth
-                  variant="contained"
-                  component={Link}
-                  to={`/house/edit/${data.id}`}
-                >
-                  Edit
-                </Button>
-                <Button
-                  fullWidth
-                  variant="outlined"
-                  color="error"
-                  onClick={() => {
-                    if (window.confirm("Are you sure?"))
-                      deleteHouseMutation.mutate(data);
-                  }}
-                >
-                  Delete
-                </Button>
-              </Stack>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Box>
-            <Typography variant="h6" color="text.secondary">
-              {data.country}
-            </Typography>
-            <Typography variant="h4" fontWeight="bold">
-              {data.address}
-            </Typography>
-            <Typography variant="h5" color="primary" sx={{ my: 2 }}>
-              {currencyFormatter.format(data.price)}
-            </Typography>
-            <Typography variant="body1" sx={{ mb: 3 }}>
-              {data.description}
-            </Typography>
+      <Container maxWidth="md" sx={{ mt: 4 }}>
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={6}>
+            <Card>
+              <CardMedia
+                component="img"
+                height="250"
+                image={data.photo || defaultImage}
+                alt="House pic"
+                sx={{ objectFit: "cover" }}
+              />
+              <CardContent>
+                <Stack direction="row" spacing={2}>
+                  <Button
+                    fullWidth
+                    variant="contained"
+                    component={Link}
+                    to={`/house/edit/${data.id}`}
+                  >
+                    Edit
+                  </Button>
+                  <Button
+                    fullWidth
+                    variant="outlined"
+                    color="error"
+                    onClick={() => {
+                      if (window.confirm("Are you sure?"))
+                        deleteHouseMutation.mutate(data);
+                    }}
+                  >
+                    Delete
+                  </Button>
+                </Stack>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Box>
+              <Typography variant="h6" color="text.secondary">
+                {data.country}
+              </Typography>
+              <Typography variant="h4" fontWeight="bold">
+                {data.address}
+              </Typography>
+              <Typography variant="h5" color="primary" sx={{ my: 2 }}>
+                {currencyFormatter.format(data.price)}
+              </Typography>
+              <Typography variant="body1" sx={{ mb: 3 }}>
+                {data.description}
+              </Typography>
 
-            <Bids house={data} />
-          </Box>
+              <Bids house={data} />
+            </Box>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Container>
     );
 };
 
